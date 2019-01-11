8df45d3658f265088bf0f2d41bf527dffec22a3e
haonan3 <whn87177868@gmail.com>
2019-01-11 18:04:26 +0800

    final test

--- test_ind.py
+++ test_ind.py
@@ -21,11 +21,11 @@ parser.add_argument('--use_feature', help = 'whether use input features', type =
 parser.add_argument('--update_emb', help = 'whether update embedding when optimizing supervised loss', type = bool, default = True)
 parser.add_argument('--layer_loss', help = 'whether incur loss on hidden layers', type = bool, default = True)
 
-parser.add_argument('--graph_path', help = 'the path of graph file', type = str, default='../author_graph_dataset/link-sub_copy.txt')
-parser.add_argument('--feature_path', help='the path of feature file', type = str, default='../author_graph_dataset/first_20_node_feature.csv')
+#parser.add_argument('--graph_path', help = 'the path of graph file', type = str, default='../author_graph_dataset/link-sub_copy.txt')
+#parser.add_argument('--feature_path', help='the path of feature file', type = str, default='../author_graph_dataset/first_20_node_feature.csv')
 
-#parser.add_argument('--graph_path', help = 'the path of graph file', type = str, default='../author_graph_dataset/author-1900-2020-link-all_copy.txt')
-#parser.add_argument('--feature_path', help='the path of feature file', type = str, default='../author_graph_dataset/node-feature.csv')
+parser.add_argument('--graph_path', help = 'the path of graph file', type = str, default='../author_graph_dataset/author-1900-2020-link-all_copy.txt')
+parser.add_argument('--feature_path', help='the path of feature file', type = str, default='../author_graph_dataset/node-feature.csv')
 
 parser.add_argument('--rel_train_path', help='the path of training relation file', type = str, default='../author_graph_dataset/5-folder-rel/rel-train1.txt')
 parser.add_argument('--rel_test_path', help='the path of testing relation file', type = str, default='../author_graph_dataset/5-folder-rel/rel-test1.txt')
@@ -55,10 +55,10 @@ def main():
     m = model(args)                                                 # initialize the model
     m.add_data(x, y, allx, graph, features, maxindex)                                   # add data
     m.build()                                                       # build the model
-    #m.init_train(init_iter_label = 10000, init_iter_graph = 400)    # pre-training
-    m.init_train(init_iter_label=1, init_iter_graph=1)  # pre-training
+    m.init_train(init_iter_label = 10000, init_iter_graph = 400)    # pre-training
+    #m.init_train(init_iter_label=1, init_iter_graph=1)  # pre-training
     iter_cnt, max_accu = 0, 0
-    while iter_cnt < 16:
+    while iter_cnt < 16000:
         m.step_train(max_iter = 10, iter_graph = 0.1, iter_inst = 1, iter_label = 0) # perform a training step
         tpy = m.predict(tx1, tx2)                                                         # predict the dev set
 
